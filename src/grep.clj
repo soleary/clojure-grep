ab484fac84bb448e26d8b30c725b97b9328e6dad
Sean O'Leary <sean@soleary.com>
2018-05-10 23:04:22 -0400

    Cleaned up and proper output

--- src/grep.clj
+++ src/grep.clj
@@ -1,16 +1,14 @@
 (ns grep
   (:require [clojure.java.io :as io]))
 
-(defn matched-lines [regex file]
+(defn print-matched-lines [regex file]
   (with-open [reader (io/reader file)]
-    (prn (filter #(re-find regex %1) (line-seq reader)))))
+    (doall
+      (map println (filter #(re-find regex %1) (line-seq reader))))))
 
 (defn process-files [regex files]
   (doseq [file files]
-    (matched-lines regex file)))
+    (print-matched-lines regex file)))
 
 (defn -main [pattern & files]
-  (do
-    (println (System/getProperty "user.dir"))
-    (process-files (re-pattern pattern) files)
-    (println "Done running")))
+  (process-files (re-pattern pattern) files))
