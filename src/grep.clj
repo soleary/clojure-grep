37c68ef715ece0cf8ed5c9e900ecfe5cda1b7a75
Sean O'Leary <sean@soleary.com>
2018-05-11 13:26:42 -0400

    Broke out some of the processing, better output

--- src/grep.clj
+++ src/grep.clj
@@ -1,14 +1,28 @@
 (ns grep
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [ clojure.string :as st]))
 
-(defn print-matched-lines [regex file]
-  (with-open [reader (io/reader file)]
+(defn preprocess
+  "Takes a seq of lines and returns little vectors of line number and line"
+  [lines]
+  (map vector (iterate inc 1) lines))
+
+(defn matches
+  "Match a vector made by preprocess. The original contents of the line are always last"
+  [regex linev]
+  (re-find regex (last linev)))
+
+(defn vecprint [v]
+  (println (st/join ":" v)))
+
+(defn print-matches [regex filename]
+  (with-open [reader (io/reader filename)]
     (doall
-      (map println (filter #(re-find regex %1) (line-seq reader))))))
+      (map vecprint (filter #(matches regex %1) (preprocess (line-seq reader)))))))
 
 (defn process-files [regex files]
-  (doseq [file files]
-    (print-matched-lines regex file)))
+  (doseq [filename files]
+    (print-matches regex filename)))
 
 (defn -main [pattern & files]
   (process-files (re-pattern pattern) files))
